44e40fdc820fbe9c2df0f0a99f116016335e636d
Nitish Varma <nitishvarma2k@gmail.com>
2024-12-25 22:16:48 +0530

    repo item card comp

--- src/components/RepoCard.tsx
+++ src/components/RepoCard.tsx
@@ -1,24 +1,44 @@
 import { GrStorage } from "react-icons/gr";
 
-export default function RepoCard() {
+export default function RepoCard({
+  repoItem,
+  lastItem,
+}: {
+  repoItem: {
+    id: number;
+    title: string;
+    type: string;
+    language: string;
+    size: number;
+    lastUpdated: number;
+  };
+  lastItem: boolean;
+}) {
   return (
-    <div className="flex flex-col border-b border-[#D5D7DA] p-4 gap-2 hover:bg-[#FAFAFA] cursor-pointer">
+    <div
+      className={`flex flex-col  ${
+        lastItem ? "" : "border-b"
+      }  border-[#D5D7DA] p-4 gap-2 hover:bg-[#FAFAFA] cursor-pointer`}
+    >
       <div className="flex gap-2 items-center">
-        <div className="font-medium ">design-system</div>
+        <div className="font-medium ">{repoItem.title}</div>
         <div className="bg-[#B2DDFF] border border-[#B2DDFF] py-0.5 px-2 text-sm rounded-full text-[#1570EF] bg-opacity-30">
-          Public
+          {repoItem.type}
         </div>
       </div>
       <div className="flex gap-8 text-[#414651] text-sm">
         <div className="flex items-center gap-2">
-          <div> React</div>{" "}
+          <div>{repoItem.language}</div>{" "}
           <div className="w-2 h-2 bg-[#1570EF] rounded-full"></div>{" "}
         </div>
         <div className="flex items-center gap-2">
           <GrStorage />
-          <p>7320 KB</p>{" "}
+          <p>{repoItem.size} KB</p>{" "}
         </div>
-        <div>Updated 1 day ago</div>
+        <div>
+          Updated {repoItem.lastUpdated}{" "}
+          {repoItem.lastUpdated > 1 ? "days" : "day"} ago
+        </div>
       </div>
     </div>
   );
