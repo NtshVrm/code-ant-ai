0e8fd55fe7da6ed0b7ec5fc1e9258593ca40b429
Nitish Varma <nitishvarma2k@gmail.com>
2024-12-25 19:04:09 +0530

    menu item comp

---
+++ src/components/MenuItem.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import RenderIcon from "./RenderIcon";
+
+export default function MenuItem({
+  menu,
+  disablebg,
+}: {
+  menu: { id: number; icon: string; title: string }[];
+  disablebg?: boolean;
+}) {
+  const [menuId, setMenuId] = useState(1);
+  return (
+    <>
+      {menu.map((item) => (
+        <div
+          key={item.id}
+          className={`${
+            item.id == menuId ? "bg-[#1570EF] text-white" : "hover:bg-[#FAFAFA]"
+          } p-2 m-2 rounded-md flex items-center gap-2 cursor-pointer `}
+          onClick={() => {
+            !disablebg ? setMenuId(item.id) : "";
+          }}
+        >
+          <RenderIcon icon={item.icon} />
+          <p>{item.title}</p>
+        </div>
+      ))}
+    </>
+  );
+}
