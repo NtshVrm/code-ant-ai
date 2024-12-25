a24b7897920517d16eaffb281fb5e0b3e74a9afa
Nitish Varma <nitishvarma2k@gmail.com>
2024-12-25 22:16:56 +0530

    repo main header

---
+++ src/components/RepoListHeader.tsx
@@ -0,0 +1,52 @@
+import { IoIosSearch } from "react-icons/io";
+import RenderIcon from "./RenderIcon";
+
+export default function RepoListHeader() {
+  const repoMenu = [
+    {
+      id: 1,
+      icon: "refresh",
+      title: "Refresh All",
+    },
+    {
+      id: 2,
+      icon: "add",
+      title: "Add Repository",
+    },
+  ];
+
+  return (
+    <header className="p-4 border-b border-[#D5D7DA] flex flex-col gap-2">
+      <div className="flex flex-col gap-2 md:flex-row md:justify-between">
+        <div className="flex flex-col gap-0.5">
+          <div className="text-2xl font-medium">Repositories</div>
+          <div className="text-sm text-[#414651]">33 total repositories</div>
+        </div>
+        <div className="flex gap-2 h-fit text-sm">
+          {repoMenu.map((item) => {
+            return (
+              <div
+                className={`flex gap-2 p-2 items-center border border-[#D5D7DA] rounded-md ${
+                  item.id == 2 ? "bg-[#1570EF] text-white" : ""
+                }`}
+              >
+                <RenderIcon icon={item.icon} />
+                <p>{item.title}</p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div className="flex items-center border border-[#D5D7DA] mt-2 p-0.5 rounded-md md:w-1/3">
+        <div className="scale-[1.5] px-2">
+          <RenderIcon icon="search" />
+        </div>
+        <input
+          type="text"
+          placeholder="Search repositories"
+          className="w-full px-2 py-1 focus:border-0 focus:outline-none"
+        />
+      </div>
+    </header>
+  );
+}
