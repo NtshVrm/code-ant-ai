cb896a11978e4fea102de702553107685ffb9d8c
Nitish Varma <nitishvarma2k@gmail.com>
2024-12-26 02:57:19 +0530

    setup react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Home";
 
 function App() {
-  return <>
-    <Dashboard />
-  </>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
